ae8ca334a8972adc66a0daa8c1e85da8da98ffc6
Jabrilo19 <foyjemyre@gmail.com>
2026-09-07 16:20:55 -0400

    Add broken code to test CI checks

---
+++ src/project/annotations/CITest.java
@@ -0,0 +1,10 @@
+package project.annotations;
+
+public class CITest {
+    public void badMethod(int a, int b, int c, int d, int e, int f, int g, int h) {
+        thisDoesNotExist();
+    }
+}
+
+
+
