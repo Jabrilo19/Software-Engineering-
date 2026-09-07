859aabf90731a7ad25ca2dec76217afa04529043
Jabrilo19 <foyjemyre@gmail.com>
2026-09-07 15:49:23 -0400

    Remove deliberate checkstyle violation

--- src/project/annotations/CheckstyleTest.java
+++
@@ -1,7 +0,0 @@
-package project.annotations;
-
-public class ChechStyleTest {
-    public void excessiveParamTest(int a, int b, int c, int d, int e, int f, int g, int h) {
-        // The limit is 7 so 8 parameters should flag the check
-    }
-}
