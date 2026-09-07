e90d49e721b615275f86e748263e87d7c2fecd22
Jabrilo19 <foyjemyre@gmail.com>
2026-09-07 16:24:10 -0400

    Fix syntax and checkstyle issues in CITest

--- src/project/annotations/CITest.java
+++ src/project/annotations/CITest.java
@@ -1,8 +1,8 @@
 package project.annotations;
 
 public class CITest {
-    public void badMethod(int a, int b, int c, int d, int e, int f, int g, int h) {
-        thisDoesNotExist();
+    public void badMethod(int a, int b) {
+        int sum = a + b;
     }
 }
 
